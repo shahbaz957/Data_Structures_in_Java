14e23b2b4d3139b23004446a7ef4d01909c043d6
Mirza Shahbaz Ali Baig <mirzashahbazbaig724@gmail.com>
2026-01-22 05:36:48 +0500

    Reversed a queue using a stack

--- Queue/Main.java
+++ Queue/Main.java
@@ -1,4 +1,6 @@
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Main{
     public static void main(String[] args){
@@ -47,15 +49,29 @@ public class Main{
         // queue.dequeue();
         // System.out.println(queue.show());
 
-        PriorityQueue queue = new PriorityQueue(10);
-        queue.enqueue(5);
-        queue.enqueue(1);
-        queue.enqueue(78);
-        queue.enqueue(90);
-        queue.dequeue();
-        queue.enqueue(54);
-        queue.dequeue();
-        System.out.println(queue.s_show());
+        // PriorityQueue queue = new PriorityQueue(10);
+        // queue.enqueue(5);
+        // queue.enqueue(1);
+        // queue.enqueue(78);
+        // queue.enqueue(90);
+        // queue.dequeue();
+        // queue.enqueue(54);
+        // queue.dequeue();
+        // System.out.println(queue.s_show());
         // so the priority queue is sth like array storing the element in asc order but it has different implementation depending upon the task that is implemented using that structure
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(34);
+        q.offer(35);
+        q.offer(36);
+        q.offer(37);
+        System.out.println(q);
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()){
+            s.push(q.poll());
+        }
+        while (!s.isEmpty()){
+            q.offer(s.pop());
+        }
+        System.out.println(q);
     }
 }
